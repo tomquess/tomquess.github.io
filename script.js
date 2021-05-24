78a72a23e19ded8fe9baeb2deeb3b4c9d30ab830
Tomquess <tomques11@gmail.com>
2021-05-24 17:17:19 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+$("header").hover(
+    function(){
+      $(this).filter(':not(:animated)').animate({
+         height:'350px'
+      },400);
+      
+      $('#logo').animate({
+        height:'300px'
+      },400);
+
+      $('#menu').animate({
+        height:'50px'
+      },400);
+      $('#menuOption').animate({
+        height:'50px'
+      },400);
+
+
+    },
+    function() {
+      $(this).animate({
+         height:'50px'
+      },200);
+
+      $('#logo').animate({
+        height:'50px'
+     },200);
+     $('#menu').animate({
+        height:'0px'
+     },200);
+     $('#menuOption').animate({
+        height:'0px'
+     },200);
+
+    });
